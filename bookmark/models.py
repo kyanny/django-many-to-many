d232fe98a5fd411dec1d64e27dd972296714c958
Kensuke Nagae <kyanny@gmail.com>
2021-01-27 00:38:39 +0900

    Create models

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Bookmark(models.Model):
+    url = models.TextField()
+    tags = models.ManyToManyField("Tag")
+
+    class Meta:
+        db_table = 'bookmark'
+
+    def __str__(self):
+        return self.url
+
+class Tag(models.Model):
+    name = models.TextField()
+
+    class Meta:
+        db_table = 'tag'
